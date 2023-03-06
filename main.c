43d973d99edff1e310dceacf14a252ee0a8cb816
ZWRMAT <111044221+ZWRMAT@users.noreply.github.com>
2023-03-06 08:05:51 +0200

    Q3.2: Counts from zero to age of the user and displays this value on the LCD of STM32

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #define STM32F051												   //COMPULSORY
 #include "stm32f0xx.h"											   //COMPULSORY
 #include "lcd_stm32f0.h"
+#include <stdio.h>
 
 // GLOBAL VARIABLES ----------------------------------------------------------|
 struct age_data
@@ -22,16 +23,29 @@ struct age_data my_age;
 // FUNCTION DECLARATIONS -----------------------------------------------------|
 
 void main(void);                                                   //COMPULSORY
+void init_LCD(void);
+void delay();
 
 // MAIN FUNCTION -------------------------------------------------------------|
 
 void main(void)
 {
+	init_LCD();
+	char age_string[3];
 
+	my_age.date = 12;
+	my_age.month = 9;
+	my_age.year = 2001;
+	my_age.age = 21;
 
 	while(1)
 	{
-
+		for(int i=0; i<=my_age.age; i++){
+			sprintf(age_string, "%d", i);
+			lcd_putstring(age_string);
+			delay(200000);
+			lcd_command(CLEAR);
+		}
 	}
 }
 
